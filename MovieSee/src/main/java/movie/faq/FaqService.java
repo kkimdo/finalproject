dafdff62ac5066d593491cef614aec3b574eaff4
DESKTOP-BINLVAR\user <rmswo3029@naver.com>
2017-11-24 20:05:51 +0900

    faq게시판 대소문자 구별

--- MovieSee/src/main/java/movie/faq/FaqService.java
+++ MovieSee/src/main/java/movie/faq/FaqService.java
@@ -1,18 +1,30 @@
 package movie.faq;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
 
+
 @Service
-public class FaqService implements FaqDAO {
+public  class FaqService implements FaqDAO {
 
 	@Resource
-	private SqlSessionTemplate sqlSessionTemplate;
+	private SqlSessionTemplate sqlSessionTemplate; 
+	
+	
+
+	@Override
+	public List<FaqModel> FaqList() { //글 목록
+		return sqlSessionTemplate.selectList("faq.faqList");
+	}
+
+
 	
 	@Override
-	public int faqWrite(FaqModel faqModel) {
+	public int FaqWrite(FaqModel faqModel) { //글 쓰기
 		return sqlSessionTemplate.insert("faq.faqWrite", faqModel);
 	}
 	
